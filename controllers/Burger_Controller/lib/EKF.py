c44889aa5b8b4fbea28f04844453f9396aab4902
RaoulBickmann <raoul.bickmann@googlemail.com>
2019-06-23 20:02:48 +0200

    .

--- controllers/Burger_Controller/lib/EKF.py
+++ controllers/Burger_Controller/lib/EKF.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sympy as sp
+from filterpy.kalman import ExtendedKalmanFilter as EKF
 
 # Matrices
 R = 0
@@ -10,3 +11,12 @@ H = 0
 
 
 #EKF
+
+def slam_Prediction(prev_mean, prev_cova, control_in, observations, map_size):
+    map_size_new = map_size
+    
+    F = np.matrix
+
+
+
+# def slam_Correction():
